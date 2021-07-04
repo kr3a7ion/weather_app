455c91a50b7fd5b2a0ac091fe2188126fa7ad694
Gideon <gideon.sunday@gmail.com>
2021-07-04 23:28:47 +0100

    Second Commit

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,5 +1,55 @@
 import 'package:flutter/material.dart';
-import 'widgets/weather_tile.dart';
+import 'widgets/main_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+Future<WeatherInfo> fetchWeather () async {
+  final zipCode = "60005";
+  final apiKey = "afbe9957c4e5d2ff1bdeb892fbc11aeb";
+  final requestUrl = "https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${apiKey}";
+
+  final response = await http.get(Uri.parse(requestUrl));
+
+  if (response.statusCode == 200) {
+    return WeatherInfo.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Error loading request URL info.");
+  }
+}
+
+class WeatherInfo {
+  final location;
+  final temp;
+  final tempMin;
+  final tempMax;
+  final weather;
+  final humidity;
+  final windSpeed;
+
+  WeatherInfo({
+    required this.location,
+    required this.temp,
+    required this.tempMin,
+    required this.tempMax,
+    required this.weather,
+    required this.humidity,
+    required this.windSpeed,
+  });
+
+  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+    return WeatherInfo(
+      location: json['name'],
+      temp: json['main']['temp'],
+      tempMin: json['main']['temp_min'],
+      tempMax: json['main']['temp_max'],
+      weather: json['weather'][0]['description'],
+      humidity: json['main']['humidity'],
+      windSpeed: json['wind']['speed']
+    );
+  }
+
+}
 
 void main() => runApp(MaterialApp(title: "Weather App", home: MyApp()));
 
@@ -11,59 +61,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyApp extends State<MyApp> {
+
+  late Future<WeatherInfo> futureWeather;
+
+  @override
+  void initState() { 
+    super.initState();
+    futureWeather = fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.width / 2,
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xfff1f1f1),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Location",
-                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Text(
-                    'Temp',
-                    style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.w900
-                    ),
-                  ),
-                ),
-                Text(
-                  'High of temp, low of temp',
-                  style: TextStyle(
-                    color: Color(0xff9e9e9e),
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w900
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: ListView(
-                children: [
-                  WeatherTile(icon: Icons.thermostat_outlined, title: 'Temperature', subtitle: '85'),
-                  WeatherTile(icon: Icons.filter_drama_outlined, title: 'Weather', subtitle: 'Cloudy'),
-                  WeatherTile(icon: Icons.wb_sunny, title: 'Humidity', subtitle: 'Something%'),
-                  WeatherTile(icon: Icons.waves_outlined, title: 'WindSpeed', subtitle: '2MPH'),
-                ],
-              ), 
-            ),
-          )
-        ],
-      ),
+      body: FutureBuilder<WeatherInfo>(
+         future: futureWeather,
+         builder: (context, snapshot) {
+           if (snapshot.hasData) {
+             return MainWidget(
+               location: snapshot.data!.location,
+               temp: snapshot.data!.temp,
+               tempMin: snapshot.data!.tempMin,
+               tempMax: snapshot.data!.tempMax,
+               weather: snapshot.data!.weather,
+               humidity: snapshot.data!.humidity,
+               windSpeed: snapshot.data!.windSpeed,
+             );
+           } else if (snapshot.hasError) {
+             return Center(
+               child: Text("${snapshot.error}"),
+             );
+           }
+           return CircularProgressIndicator();
+         },
+      )
     );
   }
 }
